ce825bfdbb8da48b292ad04b5ddc6031789cb681
joão maia <joomaia171820@gmail.com>
2022-01-31 15:36:30 -0300

    separei os arquivos de estilo e javascript dentro de pastas separadas e incrementei a função darkMode

--- script.js
+++
@@ -1,8 +0,0 @@
-const btnDark=document.querySelector(".dark-mode-btn");
-let on=false;
-btnDark.addEventListener("click", ()=> {
-
-	document.body.classList.toggle('Dark');
-
-
-});
---
+++ scripts/script.js
@@ -0,0 +1,60 @@
+const btnDark=document.querySelector(".dark-mode-btn");
+const body=document.querySelector("body");
+const card=document.querySelectorAll(".social-media-box");
+const header=document.querySelector("header");
+const span=document.querySelectorAll("span");
+const p=document.querySelectorAll("p");
+const overviewBox=document.querySelectorAll(".overview-box");
+const h1=document.querySelector("h1");
+
+
+let darkOn=true;
+
+btnDark.addEventListener("click",changeDark);
+
+
+function changeDark()
+{
+	body.style.backgroundColor="hsl(0, 0%, 100%)";
+
+	card.forEach(card=>{
+    
+    card.style.backgroundColor="hsl(227, 47%, 96%)";
+    card.style.color=" hsl(230, 17%, 14%)";
+
+	});
+
+
+	header.style.backgroundColor="hsl(227, 47%, 96%)"
+	header.style.color=" hsl(230, 17%, 14%)";
+   
+
+   span.forEach(span=>{
+    
+     span.style.color="hsl(230, 17%, 14%)"
+
+   });
+
+
+   
+   p.forEach(p=>{
+      
+      p.style.color="hsl(230, 17%, 14%)"
+
+
+   });
+
+
+
+   overviewBox.forEach(overviewBox=>{
+
+   overviewBox.style.backgroundColor="hsl(227, 47%, 96%)";
+
+
+   });
+
+
+  h1.style.color="hsl(230, 17%, 14%)";
+	
+
+}
